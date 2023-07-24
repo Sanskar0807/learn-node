315484dd82cf9559237afc6664395e7d42836a8d
Sanskar0807 <108810393+Sanskar0807@users.noreply.github.com>
2023-07-24 18:22:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the home page!');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the about page.');
+  } else if (req.url === '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('You can contact us at contact@example.com');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+const port = 5050;
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
